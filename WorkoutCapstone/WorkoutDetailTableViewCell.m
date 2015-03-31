70442cf550031874272c642b8883452797d92d9d
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-03-31 14:33:37 -0600

    Sets WorkoutDetail custom Cell

--- WorkoutCapstone/WorkoutDetailTableViewCell.m
+++ WorkoutCapstone/WorkoutDetailTableViewCell.m
@@ -14,6 +14,34 @@
     // Initialization code
 }
 
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    
+    if (self) {
+        // create label
+        self.detailWorkoutLabel = [UILabel new];
+        
+        // customize Label
+        self.detailWorkoutLabel.text = @"Workout Title to Go Here";
+        [self.detailWorkoutLabel setFont:[UIFont boldSystemFontOfSize:26.0]];
+        [self.detailWorkoutLabel setTextAlignment:NSTextAlignmentCenter];
+        [self.detailWorkoutLabel setTextColor:[UIColor blackColor]];
+        [self.detailWorkoutLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+        
+        // add to view
+        [self.contentView addSubview:self.detailWorkoutLabel];
+        
+        // AUTOLAYOUT
+        NSDictionary *viewsDictionary = NSDictionaryOfVariableBindings(_detailWorkoutLabel);
+        NSArray *constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-60-[_detailWorkoutLabel(>=250)]-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:viewsDictionary];
+        [self.contentView addConstraints:constraints];
+        NSArray *constraintsVertical = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_detailWorkoutLabel]-|" options:NSLayoutFormatAlignAllCenterY metrics:nil views:viewsDictionary];
+        [self.contentView addConstraints:constraintsVertical];
+    }
+    return self;
+}
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 
