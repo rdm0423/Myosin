ed48ff25979d1cb63668219ce5e83c945454b589
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-04-08 13:57:13 -0600

    adds NSObject ExerciseGetterController
    this will refernce the json file

---
+++ WorkoutCapstone/ExerciseGetterController.h
@@ -0,0 +1,13 @@
+//
+//  ExerciseGetterController.h
+//  WorkoutCapstone
+//
+//  Created by Ross McIlwaine on 4/8/15.
+//  Copyright (c) 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExerciseGetterController : NSObject
+
+@end
---
+++ WorkoutCapstone/ExerciseGetterController.m
@@ -0,0 +1,13 @@
+//
+//  ExerciseGetterController.m
+//  WorkoutCapstone
+//
+//  Created by Ross McIlwaine on 4/8/15.
+//  Copyright (c) 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import "ExerciseGetterController.h"
+
+@implementation ExerciseGetterController
+
+@end
