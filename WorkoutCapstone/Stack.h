32a9ec46ad56a0bfd451d4212aba2d59885e2c37
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-04-08 14:31:48 -0600

    Adds CoreData for json file

---
+++ WorkoutCapstone/Stack.h
@@ -0,0 +1,17 @@
+//
+//  Stack.m
+//
+//  Created by Joshua Howland on 6/12/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject
+
++ (Stack *)sharedInstance;
+
+@property (nonatomic, strong, readonly) NSManagedObjectContext *managedObjectContext;
+
+@end
