e2e0c0430647bfb367575704da5067b2db893a0d
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-04-10 12:40:11 -0600

    Sets settings support VC

--- WorkoutCapstone/SupportViewController.m
+++ WorkoutCapstone/SupportViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "SupportViewController.h"
+@import MessageUI;
 
-@interface SupportViewController ()
+@interface SupportViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -16,13 +17,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.title = @"Support";
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(done)];
+    [self.navigationItem.leftBarButtonItem setTintColor:[UIColor whiteColor]];
+    
+    
+    UILabel *tempLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 70, 300, 80)];
+    tempLabel.numberOfLines = 0;
+    tempLabel.text = @"Let us know if you have any questions or suggestions.";
+    
+    
+    // Sets email compose
+    UIButton *composeEmailButton = [[UIButton alloc] initWithFrame:CGRectMake(100, 350, 180, 80)];
+    composeEmailButton.tintColor = [UIColor lightGrayColor];
+    composeEmailButton.layer.borderWidth = 1.0;
+    composeEmailButton.layer.cornerRadius = 3;
+    composeEmailButton.layer.borderColor = [UIColor blueColor].CGColor;
+    [composeEmailButton setTitle:@"Send feedback" forState:UIControlStateNormal];
+    [composeEmailButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [composeEmailButton addTarget:self action:@selector(sendFeedbackEmail:) forControlEvents:UIControlEventTouchUpInside];
+    [composeEmailButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    
+    [self.view addSubview:tempLabel];
+    [self.view addSubview:composeEmailButton];
+
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)sendFeedbackEmail:(id)sender {
+    
+    MFMailComposeViewController *mailComposeViewController = [[MFMailComposeViewController alloc] init];
+    mailComposeViewController.mailComposeDelegate = self;
+#warning Change Email
+    [mailComposeViewController setToRecipients:@[@"weekendconcept@gmail.com"]];
+    [mailComposeViewController setSubject:@""];
+    [mailComposeViewController.navigationBar setTintColor:[UIColor whiteColor]];
+    
+    if ([MFMailComposeViewController canSendMail]) {
+        [self presentViewController:mailComposeViewController animated:YES completion:nil];
+    }
+}
+
+// allows user to hit 'cancel' and exit
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)done {
+    //    [self.drawerController openDrawerSide:MMDrawerSideLeft animated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 /*
 #pragma mark - Navigation
