78f7e5e26dee179a327e6cb92ee7600cf0104e7f
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-04-22 14:51:24 -0600

    creates WorkOutModeCell

---
+++ WorkoutCapstone/WorkoutModeCell.h
@@ -0,0 +1,13 @@
+//
+//  WorkoutModeCell.h
+//  WorkoutCapstone
+//
+//  Created by Ross McIlwaine on 4/22/15.
+//  Copyright (c) 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkoutModeCell : UITableViewCell
+
+@end
---
+++ WorkoutCapstone/WorkoutModeCell.m
@@ -0,0 +1,23 @@
+//
+//  WorkoutModeCell.m
+//  WorkoutCapstone
+//
+//  Created by Ross McIlwaine on 4/22/15.
+//  Copyright (c) 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import "WorkoutModeCell.h"
+
+@implementation WorkoutModeCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
