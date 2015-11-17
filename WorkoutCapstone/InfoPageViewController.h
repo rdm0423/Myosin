5db39b1a9a1fd5c40e42e6377bf324976b5dc2a5
Ross McIlwaine <rdm0423@gmail.com>
2015-11-16 18:55:19 -0700

    Updates for iOS 9

---
+++ WorkoutCapstone/InfoPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoPageViewController.h
+//  WorkoutCapstone
+//
+//  Created by Ross McIlwaine on 11/16/15.
+//  Copyright © 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoPageViewController : UIPageViewController
+
+@end
---
+++ WorkoutCapstone/InfoPageViewController.m
@@ -0,0 +1,37 @@
+//
+//  InfoPageViewController.m
+//  WorkoutCapstone
+//
+//  Created by Ross McIlwaine on 11/16/15.
+//  Copyright © 2015 Ross McIlwaine. All rights reserved.
+//
+
+#import "InfoPageViewController.h"
+
+@interface InfoPageViewController ()
+
+@end
+
+@implementation InfoPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
